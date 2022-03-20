328653b0c0f7e8a69d695212ac2107004b2e1b4a
Gabriel Hoppe <hoppecsgonet@gmail.com>
2022-03-19 21:22:43 -0300

    first commit

---
+++ Memory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mem
+{
+
+    public class Memory
+    {
+
+        [DllImport("kernel32.dll")]
+        static extern bool ReadProcessMemory(IntPtr proc, IntPtr address, byte[] buffer, int size, ref int read);
+
+        [DllImport("kernel32.dll")]
+        static extern bool WriteProcessMemory(IntPtr proc, IntPtr address, byte[] buffer, int size, ref int written);
+
+        int read, write;
+        byte[] ptr = new byte[8];
+        byte[] buffer = new byte[1024];
+
+        public IntPtr readpointer(IntPtr proc, IntPtr address)
+        {
+
+            ReadProcessMemory(proc, address, buffer, 4, ref read);
+
+            return (IntPtr)BitConverter.ToInt32(buffer, 0);
+        }
+
+
+        public byte[] readBytes(IntPtr proc, IntPtr address, int size)
+        {
+
+            ReadProcessMemory(proc, address, buffer, size, ref read);
+
+            return buffer;
+        }
+
+
+        public void writebytes(IntPtr proc, IntPtr address, byte[] newbytes)
+        {
+
+            WriteProcessMemory(proc, address, newbytes, newbytes.Length, ref write);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+using mem;
+
+namespace Hackkk
+{
+    internal class Program
+    {
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vkey);
+
+        static void Main(string[] args)
+        {
+
+            Memory memory = new Memory();
+
+            IntPtr moduleBaseAddress = IntPtr.Zero;    
+
+            Process proc = Process.GetProcessesByName("csgo")[0];
+
+            foreach ( ProcessModule module in proc.Modules )
+            {
+
+                if (module.ModuleName == "client.dll") moduleBaseAddress = module.BaseAddress;
+            }
+
+            if (moduleBaseAddress != IntPtr.Zero)
+            {
+
+                const int localPlayerOffset = 0xDB35DC;
+                const int forceJumpOffset = 0x5278DDC;
+                const int groundFlagOffset = 0x104;
+
+                IntPtr localPlayerAddress = IntPtr.Add(moduleBaseAddress, localPlayerOffset);
+                IntPtr forceJumpAddress = IntPtr.Add(moduleBaseAddress, forceJumpOffset);
+
+                IntPtr localPlayerBuffer = memory.readpointer(proc.Handle, localPlayerAddress);
+
+                IntPtr groundAdress = IntPtr.Add(localPlayerBuffer, groundFlagOffset);
+
+                Thread bhopThread = new Thread(bhop);
+                bhopThread.Start();
+
+                Console.ReadKey();
+
+                void bhop()
+                {
+
+                    while (true)
+                    {
+
+                        if (GetAsyncKeyState(Keys.Space) < 0)
+                        {
+
+                            IntPtr groundFlagBuffer = memory.readpointer(proc.Handle, groundAdress);
+
+                            int flag = (int)groundFlagBuffer;
+
+                            if (flag == 257 || flag == 263 || flag == 261)
+                            {
+
+                                memory.writebytes(proc.Handle, forceJumpAddress, BitConverter.GetBytes(5));
+                            } else
+                            {
+                                memory.writebytes(proc.Handle, forceJumpAddress, BitConverter.GetBytes(4));
+                            }
+                        }
+
+                        Thread.Sleep(3);
+                    }
+                }
+            }
+        }
+    }
+}
